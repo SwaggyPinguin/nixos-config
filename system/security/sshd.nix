8e34effe6326a8b6920518c29e9096a3dd15df87
Noah <ndahms.dev@gmail.com>
2024-03-18 21:17:43 +0100

    added sshd module

---
+++ system/security/sshd.nix
@@ -0,0 +1,22 @@
+# { userSettings, authorizedKeys ? [], ... }:
+{ config, userSettings, authorizedKeys ? [], ... }:
+# let 
+#     cfg = config.services.openssh;
+# in mkId cfg.enable 
+{
+    # Enable incoming ssh
+    services.openssh = {
+        enable = true;
+        openFirewall = true;
+        settings = {
+            PasswordAuthentication = false;
+            PermitRootLogin = "no";
+        };
+    };
+
+    services.ssh-agent.enable = true;
+
+    users.users.${userSettings.username}.openssh.authorizedKeys.keys = authorizedKeys;
+
+    programs.ssh.askForPassword = "";
+}
